f862f69feafd8d5ac9bb094fdc4494164752fcae
Thomas Joerg <tjoerg@google.com>
2024-05-13 05:51:19 -0700

    [XLA:GPU] Remove preprocessor #if in GemmRewriter.
    Pass the CUDA or ROCm version into the pass explicitly. Replace the one preprocessor #if. I plan to do the same thing for all other preprocessor #ifs.
    
    PiperOrigin-RevId: 633180367

--- third_party/xla/xla/service/gpu/gpu_compiler.cc
+++ third_party/xla/xla/service/gpu/gpu_compiler.cc
@@ -1385,6 +1385,12 @@ absl::Status GpuCompiler::OptimizeHloPostLayoutAssignment(
     }
   };
 
+#if GOOGLE_CUDA
+  constexpr int toolkit_version = CUDA_VERSION;
+#else
+  constexpr int toolkit_version = TF_ROCM_VERSION;
+#endif
+
   {
     HloPassPipeline pipeline("hlo normalization");
 
@@ -1424,14 +1430,16 @@ absl::Status GpuCompiler::OptimizeHloPostLayoutAssignment(
 
     // Rewrite FP8 GEMMs ahead of Triton which currently lacks support for FP8
     // and may rewrite quantized FP8 GEMMs as higher-precision GEMMs.
-    pipeline.AddPass<GemmRewriter>(gpu_version, /*f8_rewrite=*/true);
+    pipeline.AddPass<GemmRewriter>(gpu_version, toolkit_version,
+                                   /*f8_rewrite=*/true);
     if (debug_options.xla_gpu_enable_triton_gemm() && cuda_cc != nullptr &&
         cuda_cc->IsAtLeast(se::CudaComputeCapability::AMPERE)) {
       pipeline.AddPass<GemvRewriter>();
       pipeline.AddPass<GemmFusion>(gpu_version);
     }
     // Rewrite non-FP8 GEMMs.
-    pipeline.AddPass<GemmRewriter>(gpu_version, /*f8_rewrite=*/false);
+    pipeline.AddPass<GemmRewriter>(gpu_version, toolkit_version,
+                                   /*f8_rewrite=*/false);
 
     // Rewrite GEMMs with broadcasted inputs as strided GEMMs.
     pipeline.AddPass<GemmBroadcastFoldingRewriter>();
@@ -1496,7 +1504,7 @@ absl::Status GpuCompiler::OptimizeHloPostLayoutAssignment(
   pipeline.AddPass<CallInliner>();
   // TODO(tdanyluk): Apply CublasPadForGemms to the cuBLAS GEMMs generated
   // here for possibly better cuBLAS performance.
-  pipeline.AddPass<GemmRewriter>(gpu_version);
+  pipeline.AddPass<GemmRewriter>(gpu_version, toolkit_version);
   // Rewrite GEMMs with broadcasted inputs as strided GEMMs.
   pipeline.AddPass<GemmBroadcastFoldingRewriter>();
 
